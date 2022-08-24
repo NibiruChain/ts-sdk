71dbce98ffac29dd2f6cc20e17e5a949d5aaf971
Unique Divine <51418232+Unique-Divine@users.noreply.github.com>
2022-08-24 02:51:20 -0500

    fix: rename HOST to CHAIN_HOST because HOST is reset by CRA (#7)
    * bump version minor
    
    * bump version minor
    
    * fix: ci secrets update
    
    * fix(packages): Update dependencies

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,4 @@
-export { PageRequest, Coin } from './common'
-export * from './network'
-export * from './constants'
-export * from './utils'
+export { PageRequest, Coin } from "./common"
+export * from "./network"
+export * from "./constants"
+export * from "./utils"
