605b383367cc0bb7c44a28f363501c09a6b67968
Unique-Divine <realuniquedivine@gmail.com>
2022-08-24 02:44:01 -0500

    fix(packages): Update dependencies

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,4 @@
 export { PageRequest, Coin } from "./common"
 export * from "./network"
 export * from "./constants"
-export { fromSdkDec, fromSdkInt, toSdkDec, toSdkInt } from "./utils"
+export * from "./utils"
