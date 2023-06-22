d5314745f31ee30b61728c6e79785e4c2b3485b5
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-06-22 13:12:28 -0400

    chore: generate v0.21.0 protos

---
+++ packages/protojs/src/nibiru/epochs/v1/state.ts
@@ -0,0 +1,262 @@
+/* eslint-disable */
+import Long from "long"
+import _m0 from "protobufjs/minimal"
+import { Duration } from "../../../google/protobuf/duration"
+import { Timestamp } from "../../../google/protobuf/timestamp"
+
+export const protobufPackage = "nibiru.epochs.v1"
+
+export interface EpochInfo {
+  /** A string identifier for the epoch. e.g. "15min" or "1hour" */
+  identifier: string
+  /** When the epoch repetitino should start. */
+  startTime?: Date
+  /** How long each epoch lasts for. */
+  duration?: Duration
+  /** The current epoch number, starting from 1. */
+  currentEpoch: Long
+  /** The start timestamp of the current epoch. */
+  currentEpochStartTime?: Date
+  /**
+   * Whether or not this epoch has started. Set to true if current blocktime >=
+   * start_time.
+   */
+  epochCountingStarted: boolean
+  /** The block height at which the current epoch started at. */
+  currentEpochStartHeight: Long
+}
+
+function createBaseEpochInfo(): EpochInfo {
+  return {
+    identifier: "",
+    startTime: undefined,
+    duration: undefined,
+    currentEpoch: Long.UZERO,
+    currentEpochStartTime: undefined,
+    epochCountingStarted: false,
+    currentEpochStartHeight: Long.ZERO,
+  }
+}
+
+export const EpochInfo = {
+  encode(message: EpochInfo, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.identifier !== "") {
+      writer.uint32(10).string(message.identifier)
+    }
+    if (message.startTime !== undefined) {
+      Timestamp.encode(
+        toTimestamp(message.startTime),
+        writer.uint32(18).fork(),
+      ).ldelim()
+    }
+    if (message.duration !== undefined) {
+      Duration.encode(message.duration, writer.uint32(26).fork()).ldelim()
+    }
+    if (!message.currentEpoch.isZero()) {
+      writer.uint32(32).uint64(message.currentEpoch)
+    }
+    if (message.currentEpochStartTime !== undefined) {
+      Timestamp.encode(
+        toTimestamp(message.currentEpochStartTime),
+        writer.uint32(42).fork(),
+      ).ldelim()
+    }
+    if (message.epochCountingStarted === true) {
+      writer.uint32(48).bool(message.epochCountingStarted)
+    }
+    if (!message.currentEpochStartHeight.isZero()) {
+      writer.uint32(56).int64(message.currentEpochStartHeight)
+    }
+    return writer
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): EpochInfo {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseEpochInfo()
+    while (reader.pos < end) {
+      const tag = reader.uint32()
+      switch (tag >>> 3) {
+        case 1:
+          if (tag !== 10) {
+            break
+          }
+
+          message.identifier = reader.string()
+          continue
+        case 2:
+          if (tag !== 18) {
+            break
+          }
+
+          message.startTime = fromTimestamp(Timestamp.decode(reader, reader.uint32()))
+          continue
+        case 3:
+          if (tag !== 26) {
+            break
+          }
+
+          message.duration = Duration.decode(reader, reader.uint32())
+          continue
+        case 4:
+          if (tag !== 32) {
+            break
+          }
+
+          message.currentEpoch = reader.uint64() as Long
+          continue
+        case 5:
+          if (tag !== 42) {
+            break
+          }
+
+          message.currentEpochStartTime = fromTimestamp(
+            Timestamp.decode(reader, reader.uint32()),
+          )
+          continue
+        case 6:
+          if (tag !== 48) {
+            break
+          }
+
+          message.epochCountingStarted = reader.bool()
+          continue
+        case 7:
+          if (tag !== 56) {
+            break
+          }
+
+          message.currentEpochStartHeight = reader.int64() as Long
+          continue
+      }
+      if ((tag & 7) === 4 || tag === 0) {
+        break
+      }
+      reader.skipType(tag & 7)
+    }
+    return message
+  },
+
+  fromJSON(object: any): EpochInfo {
+    return {
+      identifier: isSet(object.identifier) ? String(object.identifier) : "",
+      startTime: isSet(object.startTime)
+        ? fromJsonTimestamp(object.startTime)
+        : undefined,
+      duration: isSet(object.duration) ? Duration.fromJSON(object.duration) : undefined,
+      currentEpoch: isSet(object.currentEpoch)
+        ? Long.fromValue(object.currentEpoch)
+        : Long.UZERO,
+      currentEpochStartTime: isSet(object.currentEpochStartTime)
+        ? fromJsonTimestamp(object.currentEpochStartTime)
+        : undefined,
+      epochCountingStarted: isSet(object.epochCountingStarted)
+        ? Boolean(object.epochCountingStarted)
+        : false,
+      currentEpochStartHeight: isSet(object.currentEpochStartHeight)
+        ? Long.fromValue(object.currentEpochStartHeight)
+        : Long.ZERO,
+    }
+  },
+
+  toJSON(message: EpochInfo): unknown {
+    const obj: any = {}
+    message.identifier !== undefined && (obj.identifier = message.identifier)
+    message.startTime !== undefined && (obj.startTime = message.startTime.toISOString())
+    message.duration !== undefined &&
+      (obj.duration = message.duration ? Duration.toJSON(message.duration) : undefined)
+    message.currentEpoch !== undefined &&
+      (obj.currentEpoch = (message.currentEpoch || Long.UZERO).toString())
+    message.currentEpochStartTime !== undefined &&
+      (obj.currentEpochStartTime = message.currentEpochStartTime.toISOString())
+    message.epochCountingStarted !== undefined &&
+      (obj.epochCountingStarted = message.epochCountingStarted)
+    message.currentEpochStartHeight !== undefined &&
+      (obj.currentEpochStartHeight = (
+        message.currentEpochStartHeight || Long.ZERO
+      ).toString())
+    return obj
+  },
+
+  create<I extends Exact<DeepPartial<EpochInfo>, I>>(base?: I): EpochInfo {
+    return EpochInfo.fromPartial(base ?? {})
+  },
+
+  fromPartial<I extends Exact<DeepPartial<EpochInfo>, I>>(object: I): EpochInfo {
+    const message = createBaseEpochInfo()
+    message.identifier = object.identifier ?? ""
+    message.startTime = object.startTime ?? undefined
+    message.duration =
+      object.duration !== undefined && object.duration !== null
+        ? Duration.fromPartial(object.duration)
+        : undefined
+    message.currentEpoch =
+      object.currentEpoch !== undefined && object.currentEpoch !== null
+        ? Long.fromValue(object.currentEpoch)
+        : Long.UZERO
+    message.currentEpochStartTime = object.currentEpochStartTime ?? undefined
+    message.epochCountingStarted = object.epochCountingStarted ?? false
+    message.currentEpochStartHeight =
+      object.currentEpochStartHeight !== undefined &&
+      object.currentEpochStartHeight !== null
+        ? Long.fromValue(object.currentEpochStartHeight)
+        : Long.ZERO
+    return message
+  },
+}
+
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Long
+  ? string | number | Long
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>
+
+type KeysOfUnion<T> = T extends T ? keyof T : never
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & {
+      [K in Exclude<keyof I, KeysOfUnion<P>>]: never
+    }
+
+function toTimestamp(date: Date): Timestamp {
+  const seconds = numberToLong(date.getTime() / 1_000)
+  const nanos = (date.getTime() % 1_000) * 1_000_000
+  return { seconds, nanos }
+}
+
+function fromTimestamp(t: Timestamp): Date {
+  let millis = (t.seconds.toNumber() || 0) * 1_000
+  millis += (t.nanos || 0) / 1_000_000
+  return new Date(millis)
+}
+
+function fromJsonTimestamp(o: any): Date {
+  if (o instanceof Date) {
+    return o
+  } else if (typeof o === "string") {
+    return new Date(o)
+  } else {
+    return fromTimestamp(Timestamp.fromJSON(o))
+  }
+}
+
+function numberToLong(number: number) {
+  return Long.fromNumber(number)
+}
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any
+  _m0.configure()
+}
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined
+}
---
+++ packages/protojs/src/nibiru/oracle/v1/event.ts
@@ -0,0 +1,139 @@
+/* eslint-disable */
+import Long from "long"
+import _m0 from "protobufjs/minimal"
+
+export const protobufPackage = "nibiru.oracle.v1"
+
+/** Emitted when a price is posted */
+export interface OraclePriceUpdate {
+  pair: string
+  price: string
+  timestampMs: Long
+}
+
+function createBaseOraclePriceUpdate(): OraclePriceUpdate {
+  return { pair: "", price: "", timestampMs: Long.ZERO }
+}
+
+export const OraclePriceUpdate = {
+  encode(
+    message: OraclePriceUpdate,
+    writer: _m0.Writer = _m0.Writer.create(),
+  ): _m0.Writer {
+    if (message.pair !== "") {
+      writer.uint32(10).string(message.pair)
+    }
+    if (message.price !== "") {
+      writer.uint32(18).string(message.price)
+    }
+    if (!message.timestampMs.isZero()) {
+      writer.uint32(24).int64(message.timestampMs)
+    }
+    return writer
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): OraclePriceUpdate {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input)
+    let end = length === undefined ? reader.len : reader.pos + length
+    const message = createBaseOraclePriceUpdate()
+    while (reader.pos < end) {
+      const tag = reader.uint32()
+      switch (tag >>> 3) {
+        case 1:
+          if (tag !== 10) {
+            break
+          }
+
+          message.pair = reader.string()
+          continue
+        case 2:
+          if (tag !== 18) {
+            break
+          }
+
+          message.price = reader.string()
+          continue
+        case 3:
+          if (tag !== 24) {
+            break
+          }
+
+          message.timestampMs = reader.int64() as Long
+          continue
+      }
+      if ((tag & 7) === 4 || tag === 0) {
+        break
+      }
+      reader.skipType(tag & 7)
+    }
+    return message
+  },
+
+  fromJSON(object: any): OraclePriceUpdate {
+    return {
+      pair: isSet(object.pair) ? String(object.pair) : "",
+      price: isSet(object.price) ? String(object.price) : "",
+      timestampMs: isSet(object.timestampMs)
+        ? Long.fromValue(object.timestampMs)
+        : Long.ZERO,
+    }
+  },
+
+  toJSON(message: OraclePriceUpdate): unknown {
+    const obj: any = {}
+    message.pair !== undefined && (obj.pair = message.pair)
+    message.price !== undefined && (obj.price = message.price)
+    message.timestampMs !== undefined &&
+      (obj.timestampMs = (message.timestampMs || Long.ZERO).toString())
+    return obj
+  },
+
+  create<I extends Exact<DeepPartial<OraclePriceUpdate>, I>>(
+    base?: I,
+  ): OraclePriceUpdate {
+    return OraclePriceUpdate.fromPartial(base ?? {})
+  },
+
+  fromPartial<I extends Exact<DeepPartial<OraclePriceUpdate>, I>>(
+    object: I,
+  ): OraclePriceUpdate {
+    const message = createBaseOraclePriceUpdate()
+    message.pair = object.pair ?? ""
+    message.price = object.price ?? ""
+    message.timestampMs =
+      object.timestampMs !== undefined && object.timestampMs !== null
+        ? Long.fromValue(object.timestampMs)
+        : Long.ZERO
+    return message
+  },
+}
+
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Long
+  ? string | number | Long
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>
+
+type KeysOfUnion<T> = T extends T ? keyof T : never
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & {
+      [K in Exclude<keyof I, KeysOfUnion<P>>]: never
+    }
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any
+  _m0.configure()
+}
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined
+}
