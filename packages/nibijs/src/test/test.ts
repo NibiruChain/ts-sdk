a87249595f20693927ea9b241ed720e90fcb2101
elshenak <elshenak@gmail.com>
2023-08-13 17:16:53 -0400

    refactor: removing unused helper fns

--- packages/nibijs/src/test/test.ts
+++
@@ -1,9 +0,0 @@
-fetch(
-  "https://www.google.com/recaptcha/api2/reload?k=6LeEnLkmAAAAAEnJPIawDj2uxkWM7BjsErRz_Oi-",
-  {
-    method: "post",
-    headers: {},
-  }
-).then((response) => {
-  console.log(JSON.stringify(response))
-})
