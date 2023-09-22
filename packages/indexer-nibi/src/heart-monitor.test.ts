8a8b88abb047f828f36baf578eda5941c5b3a69f
elshenak <elshenak@gmail.com>
2023-09-22 12:55:26 -0400

    test: removing forced change

--- packages/indexer-nibi/src/heart-monitor.test.ts
+++ packages/indexer-nibi/src/heart-monitor.test.ts
@@ -14,8 +14,6 @@ const heartMonitor = new HeartMonitor({
   endptTm: "https://hm-graphql.itn-2.nibiru.fi",
 })
 
-// forcing a change
-
 describe("Heart Monitor constructor", () => {
   interface TestCase {
     name: string
