7a4439c3ea3136445fa7462d11e745ab3a3b3bb6
Cameron Gilbert <gilbertjcameron@gmail.com>
2023-10-12 15:11:48 -0400

    fix(fix): issue

--- jest.config.js
+++ jest.config.js
@@ -40,7 +40,7 @@ module.exports = function (root = __dirname) {
 
     // A list of reporter names that Jest uses when writing coverage reports
     // coverageReporters: ['json', 'html', 'lcov', 'text'],
-    coverageReporters: ["json", "text", "html"],
+    coverageReporters: ["json-summary", "text", "html"],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: null,
