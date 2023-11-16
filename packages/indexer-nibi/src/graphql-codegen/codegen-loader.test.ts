c89407fb990a4ae58edf3e3d79a8aa693c8fadb3
elshenak <elshenak@gmail.com>
2023-11-16 14:02:37 -0500

    test: jest timeout removed

--- packages/indexer-nibi/src/graphql-codegen/codegen-loader.test.ts
+++ packages/indexer-nibi/src/graphql-codegen/codegen-loader.test.ts
@@ -3,7 +3,6 @@ import { GraphQLSchema } from "graphql"
 const loader = require("./codegen-loader")
 
 describe("codegen-loader tests", () => {
-  jest.setTimeout(30000)
   test("codegen-loader", async () => {
     const testResult = await loader()
     expect(testResult).toBeInstanceOf(GraphQLSchema)
