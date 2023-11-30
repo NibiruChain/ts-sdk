eb961c01dfa7fd0827a0a46b6190010c54c7272e
Cameron Gilbert <gilbertjcameron@gmail.com>
2023-11-30 13:20:29 -0500

    fix(fix): fix

--- packages/nibijs/src/test/query.tests/perp.test.ts
+++ packages/nibijs/src/test/query.tests/perp.test.ts
@@ -74,9 +74,9 @@ describe("setupPerpExtension", () => {
         trader: "Test Trader",
       })
       expect(result).toEqual({
-        positionNotional: "100",
-        unrealizedPnl: "50",
-        marginRatio: "5",
+        marginRatio: "5e-18",
+        positionNotional: "1e-16",
+        unrealizedPnl: "5e-17",
       })
     })
   })
