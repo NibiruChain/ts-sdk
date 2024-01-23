caad35decb7851c89a93c8214afec20e6159b7a5
Cameron Gilbert <gilbertjcameron@gmail.com>
2024-01-23 10:41:00 -0500

    fix: path resolution (#294)

--- src/sdk/msg/encode-types.ts
+++ src/sdk/msg/encode-types.ts
@@ -1,8 +1,17 @@
 import { EncodeObject } from "@cosmjs/proto-signing"
+import { PerpMsgFactory } from "./perp"
+import { SpotMsgFactory } from "./spot"
 
 export interface MsgTypeUrls {
   [msg: string]: string
 }
+
+export declare class MsgFactory {
+  spot: typeof SpotMsgFactory
+  perp: typeof PerpMsgFactory
+}
+
+export declare const Msg: MsgFactory
 
 /** TxMessage: a message contained within a transaction.
  *
