37743c112851af9098f6d297c6fc75e14ce1a4dc
Cameron Gilbert <gilbertjcameron@gmail.com>
2024-02-05 16:59:49 -0500

    fix: another fix

--- src/gql/mutation/marketing.ts
+++ src/gql/mutation/marketing.ts
@@ -25,7 +25,7 @@ export const marketingMutationString = (
     marketing {
       ${gqlQuery(
         "updateTwitterUser",
-        args,
+        args.updateTwitterUser ?? {},
         fields
           ? convertObjectToPropertiesString(fields)
           : convertObjectToPropertiesString(defaultTwitterUser),
