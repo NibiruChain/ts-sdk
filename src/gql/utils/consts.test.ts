4cf6d19cbd3d51b76d04eb8eabe5a8495adb91ee
Ruslan <ruslan.rsh.8@gmail.com>
2024-04-27 18:05:03 +0200

    test: gql batch handler

--- src/gql/utils/consts.test.ts
+++ src/gql/utils/consts.test.ts
@@ -4,23 +4,44 @@ import {
   GqlOutCommunityPool,
   communityPoolQueryString,
   arg,
+  GQLDelegation,
+  GQLStakingQueryString,
 } from ".."
 
 describe("queryBatchHandler tests", () => {
   test("queryBatchHandler", async () => {
     const resp = await queryBatchHandler<{
       communityPool: GqlOutCommunityPool[]
+      staking: {
+        delegations: GQLDelegation[]
+      }
     }>(
-      [communityPoolQueryString({}, true)],
+      [
+        communityPoolQueryString({}, true),
+        GQLStakingQueryString(
+          { delegations: { limit: 1 } },
+          {
+            delegations: {
+              delegator: { address: "" },
+              amount: 0,
+            },
+          }
+        ),
+      ],
       "https://hm-graphql.testnet-1.nibiru.fi/query"
     )
 
     expect(resp).toHaveProperty("communityPool")
+    expect(resp.staking).toHaveProperty("delegations")
 
     if (resp.communityPool?.length) {
       const [communityPool] = resp.communityPool
       const communityPoolFields = ["amount", "denom"]
       checkFields([communityPool], communityPoolFields)
+    }
+    if (resp.staking.delegations?.length) {
+      const [delegation] = resp.staking.delegations
+      checkFields([delegation], ["amount", "delegator"])
     }
   })
 
