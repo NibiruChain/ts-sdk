42517098bba1cebb120e2b397f4ffc03657ea8d4
Cameron Gilbert <gilbertjcameron@gmail.com>
2024-06-24 12:41:32 -0700

    feat: staking extension and git submodules (#360)
    * feat: staking extension and git submodules
    
    * fix: build
    
    * fix: test build
    
    * fix: build
    
    * fix: test
    
    * fix: save
    
    * fix: submodule
    
    * fix: fix
    
    * fix: build
    
    * fix: rem comment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 set -e
 
-# Test for ./nibiru/ directory
-if [ -d "./nibiru/" ]; then
-  echo "The ./nibiru/ directory exists."
-  cd nibiru
-  git checkout main
-  cd ..
-elif [ -d "../nibiru/" ]; then
-  cd ../nibiru
-  git checkout main
-  git pull
-  cd ../ts-sdk
-else
-  cd ..
-  git clone git@github.com:NibiruChain/nibiru.git
-  cd ts-sdk
-fi
+git submodule init
+git submodule update
 
 yarn gql-generate & yarn proto-gen
 yarn build:tsc
