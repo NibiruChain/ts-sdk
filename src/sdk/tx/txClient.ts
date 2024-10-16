55956b6005000247530162bba0c51dc362040a77
elshenak <elshenak@gmail.com>
2024-10-16 02:01:01 -0400

    refactor: adding nibi account parser to nibi signingcosmwasmclient

--- src/sdk/tx/txClient.ts
+++ src/sdk/tx/txClient.ts
@@ -60,6 +60,7 @@ export class NibiruTxClient extends SigningStargateClient {
       signer,
       {
         gasPrice: GasPrice.fromString("0.025unibi"),
+        accountParser: accountFromNibiru,
         ...wasmOptions,
       }
     )
